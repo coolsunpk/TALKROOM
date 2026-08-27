49b16b2ec113f4ea3c09c139d80da85d3603d740
coolsunpk <sufyanp1121@gmail.com>
2026-08-28 03:33:32 +0500

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://esgnymnhtbxveuxhyqif.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_oKbnJrJE-91ko-0pR6fWQg_6r3J-Dnc";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
